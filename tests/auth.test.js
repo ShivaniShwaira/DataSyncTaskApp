75b19c81983fe10578abbdc59ce6492e898b83e8
Shivani Adimulam <shivani.adimulam@shwaira.com>
2025-11-13 12:43:49 +0530

    Body changed for register api testcase

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -31,49 +31,49 @@ if (server) {
         await userModel.deleteMany({});
     })
 
-    // test("POST /registration-> should register a new user successfully",async()=>{
-    //     const res= await request(app)
-    //     .post("/registration")
-    //     .send({
-    //         email:"test@exa.com",
-    //         password:"password123",
-    //         deviceId:"device123",
-    //         deviceType:"mobile",
-    //         userName:"Shiv101",
-    //         phoneNumber:"0290902891",
-    //         role:"primary",
-    //     })
-    //     expect(res.statusCode).toBe(201);
-    //     expect(res.body.status).toBe(true);
-    //     expect(res.body.data.email).toBe("test@exa.com");
-    //     expect(res.body.message).toMatch("User created successfully");
-    // })
+    test("POST /registration-> should register a new user successfully",async()=>{
+        const res= await request(app)
+        .post("/registration")
+        .send({
+            email: "test@exa.com",
+        password: "password123",
+        deviceId: "device123",
+        deviceType: "mobile",
+        userName: "Shiv101",
+        phoneNumber: "0290902891",
+        role: "primary",
+        })
+        expect(res.statusCode).toBe(201);
+        expect(res.body.status).toBe(true);
+        expect(res.body.data.email).toBe("test@exa.com");
+        expect(res.body.message).toMatch("User created successfully");
+    })
 
-    //  test("POST /login-> should user login successfully",async()=>{
-    //      await request(app)
-    // .post("/registration")
-    // .send({
-    //         email:"test@exa.com",
-    //         password:"password123",
-    //         deviceId:"device123",
-    //         deviceType:"mobile",
-    //         userName:"Shiv101",
-    //         phoneNumber:"0290902891",
-    //         role:"primary",
-    // });
-    //     const res= await request(app)
-    //     .post("/login")
-    //     .send({
-    //         email:"test@exa.com",
-    //         password:"password123",
-    //         deviceId:"device123",
-    //         deviceType:"mobile"
-    //     })
-    //     expect(res.statusCode).toBe(200);
-    //     expect(res.body.status).toBe(true);
-    //     // expect(res.body.data.email).toBe("test@exa.com");
-    //     expect(res.body.data.token).toBeDefined();
-    //     expect(res.body.message).toMatch("User logged in successfully");
-    //     process.env.TEST_TOKEN=res.body.data.token;
-    // })
+     test("POST /login-> should user login successfully",async()=>{
+         await request(app)
+    .post("/registration")
+    .send({
+            email:"test@exa.com",
+            password:"password123",
+            deviceId:"device123",
+            deviceType:"mobile",
+            userName:"Shiv101",
+            phoneNumber:"0290902891",
+            role:"primary",
+    });
+        const res= await request(app)
+        .post("/login")
+        .send({
+            email:"test@exa.com",
+            password:"password123",
+            deviceId:"device123",
+            deviceType:"mobile"
+        })
+        expect(res.statusCode).toBe(200);
+        expect(res.body.status).toBe(true);
+        // expect(res.body.data.email).toBe("test@exa.com");
+        expect(res.body.data.token).toBeDefined();
+        expect(res.body.message).toMatch("User logged in successfully");
+        process.env.TEST_TOKEN=res.body.data.token;
+    })
 })
