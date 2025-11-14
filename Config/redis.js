fad27d99f381a6361044dd60f5ef7294a765f0f8
Shivani Adimulam <shivani.adimulam@shwaira.com>
2025-11-14 12:06:27 +0530

    redis cluster added

--- Config/redis.js
+++ Config/redis.js
@@ -4,6 +4,8 @@ const { Redis } = require('ioredis');
 const connection = new Redis({
   host: process.env.REDIS_HOST || '127.0.0.1',
   port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD,   // ✅ REQUIRED for Redis Cloud
+  // tls: {},  // required for Redis Cloud
   maxRetriesPerRequest: null,   // ✅ REQUIRED by BullMQ
   enableReadyCheck: false,      // ✅ helps avoid startup delays
 });
